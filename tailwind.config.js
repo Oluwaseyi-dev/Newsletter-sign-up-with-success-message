339d0f1631f768572073724512b52370d9c96db6
Seyi <oluwaseyiodunaike675@gmail.com>
2023-09-01 18:48:40 +0100

    ohh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./style.css", "./index.html", "./app.js", "./dist/output.css"],
+  content: ["./newsletter-sign-up-with-success-message-main/style.css", "./newsletter-sign-up-with-success-message-main/index.html", "./newsletter-sign-up-with-success-message-main.app.js", "./newsletter-sign-up-with-success-message-main/dist/output.css"],
   theme: {
     extend: {
       colors: {
